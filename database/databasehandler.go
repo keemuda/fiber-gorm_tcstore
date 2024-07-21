83f6253be74af3fb6321db23c65076d4c437dea6
keemuda <75478830+keemuda@users.noreply.github.com>
2024-07-21 18:13:18 +0700

    add method connection in databasehandler

--- database/databasehandler.go
+++ database/databasehandler.go
@@ -1,6 +1,26 @@
 package database
 
 //import gorm here and connect to mysql
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DBCon *gorm.DB
+
+func ConnectDB() {
+	dsn := "root:root@tcp(127.0.0.1:3308)/tcschema?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+	}
+	log.Println("connected")
+	DBCon = db
+}
+
+/*
 
 func SearchAll(){
 
@@ -21,4 +41,4 @@ func UpdateDatatc(){
 func DeleteDatatc(){
 
 }
-
+*/
